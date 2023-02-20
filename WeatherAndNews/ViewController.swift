401ae624581c6e87927d1c7a7832e53f0baee9d0
chulyeon <guinness987@gmail.com>
2023-02-20 09:57:12 +0900

    Initial Commit

---
+++ WeatherAndNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAndNews
+//
+//  Created by chulyeon kim on 2023/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
